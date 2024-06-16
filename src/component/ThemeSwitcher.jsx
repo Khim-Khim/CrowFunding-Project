ca5bddbf6ace011aee1aaa3a468f5f4fae12ccc5
Khim-khim <thaikhiem259@gmail.com>
2024-06-16 13:27:59 +0700

    refator icon

--- src/component/ThemeSwitcher.jsx
+++ src/component/ThemeSwitcher.jsx
@@ -1,15 +1,27 @@
 import useThemeSwitcher from "../hooks/useThemeSwitcher";
+
 export const ThemeSwitcher = () => {
   const [activeTheme, setTheme] = useThemeSwitcher();
+
   return (
     <div
-      className="  md:left-[7%] lg:left-[10%] xl:left-[20%] left-[2%] z-[9999] cursor-pointer backdrop-blur-xl rounded-full"
+      className="md:left-[7%] lg:left-[10%] xl:left-[20%] left-[2%] z-[9999] cursor-pointer backdrop-blur-xl rounded-full"
       onClick={() => setTheme(activeTheme)}
     >
       {activeTheme === "light" ? (
-        <img alt="" src="/svgs/moon.svg" className="w-[40px] h-[40px]" />
+        <img
+          alt="moon"
+          src="/svgs/moon.svg"
+          className="w-[40px] h-[40px]"
+          style={{ fill: "#1dc071" }} 
+        />
       ) : (
-        <img alt="" src="/svgs/sun.svg" className="w-[40px] h-[40px]" />
+        <img
+          alt="sun"
+          src="/svgs/sun.svg"
+          className="w-[40px] h-[40px]"
+          style={{ fill: "#8c6dfd" }} 
+        />
       )}
     </div>
   );
